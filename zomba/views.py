6c6820f614b6914d5cacbefa2c7fbcca01c7630f
tomgough21 <tomgough96@gmail.com>
2016-03-15 15:45:04 +0000

    Now 3 leaderboards

--- zomba/views.py
+++ zomba/views.py
@@ -20,8 +20,14 @@ def index(request):
 def about(request):
 	return render(request, 'zomba/about.html')
 
-def leaderboard(request):
-    return render(request, 'zomba/leaderboard.html')
+def leaderboard_kills(request):
+    return render(request, 'zomba/leaderboard_kills.html')
+
+def leaderboard_survived(request):
+    return render(request, 'zomba/leaderboard_survived.html')
+
+def leaderboard_team(request):
+    return render(request, 'zomba/leaderboard_team.html')
 
 @login_required
 def create_player(request):
